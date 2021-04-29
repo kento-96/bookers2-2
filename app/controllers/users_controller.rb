163f663b1f9dc178a4d541543ea6677a4d03e53c
kento-96 <kento.soccer.1107@gmail.com>
2021-04-29 06:15:29 +0000

    [create]user_show_view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,17 @@ def index
   @book=Book.new
   @users=User.all
   @user=current_user
-  
+
 end
 
 def create
 end
 
 def show
+  @user=User.find(params[:id])
+  @book=Book.new
+  @books=@user.books
+
 end
 
 def edit
