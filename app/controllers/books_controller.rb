ac54df1ca38a64f817e84264a1a08b0599057b23
kento-96 <kento.soccer.1107@gmail.com>
2021-04-29 07:31:02 +0000

    [create]book_edit_view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,9 +21,13 @@ def show
 end
 
 def edit
+  @book=Book.find(params[:id])
 end
 
 def update
+  @book=Book.find(params[:id])
+  @book.update(book_params)
+  redirect_to book_path(@book.id)
 end
 
 def destroy
